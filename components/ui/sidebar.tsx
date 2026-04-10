9fd00c8c1131a68d60c05d0e2b5ec923ab29f9f7
afrodeenoff-9834 <afrodeen.off@gmail.com>
2026-04-10 20:36:38 +0530

    side

--- components/ui/sidebar.tsx
+++ components/ui/sidebar.tsx
@@ -197,7 +197,7 @@ function Sidebar({
 
   return (
     <div
-      className="group-peer hidden text-sidebar-foreground md:block"
+      className="group peer hidden text-sidebar-foreground md:block"
       data-state={state}
       data-collapsible={state === "collapsed" ? collapsible : ""}
       data-variant={variant}
