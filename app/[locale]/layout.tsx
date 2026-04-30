41275f4f0c951e80980cc8736187564b16483bd0
Timon <uomarafrodeen@timons-MacBook.local>
2026-05-01 03:01:44 +0530

    l

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,5 +1,8 @@
 import { Suspense } from "react"
-import LocaleLayoutContent from "./layout-content"
+import { setStaticParamsLocale } from "next-international/server"
+import { I18nProviderClient } from "@/locales/client"
+import ConsentBannerLazy from "@/components/lazy/consent-banner-lazy"
+import { LOCALE_SOFT_BORDER_STYLE } from "@/lib/constants/layout"
 
 function PageFallback() {
   return (
@@ -9,15 +12,21 @@ function PageFallback() {
   )
 }
 
-export default function LocaleLayout(props: {
+export default async function LocaleLayout(props: {
   params: Promise<{ locale: string }>
   children: React.ReactNode
 }) {
+  const { locale } = await props.params
+  setStaticParamsLocale(locale)
+
   return (
     <Suspense fallback={<PageFallback />}>
-      <LocaleLayoutContent>
-        {props.children}
-      </LocaleLayoutContent>
+      <I18nProviderClient locale={locale}>
+        <div className="qe-no-white-borders flex flex-1 flex-col" style={LOCALE_SOFT_BORDER_STYLE}>
+          <ConsentBannerLazy />
+          {props.children}
+        </div>
+      </I18nProviderClient>
     </Suspense>
   )
 }
